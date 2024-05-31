ba00b64f88ab1d7379167af0dce11c18dec7a5d9
Victor N Anim <victor.nyarko@ymail.com>
2024-05-31 05:29:09 +0000

    streamlit msg

--- scr/app.py
+++ scr/app.py
@@ -1,4 +1,4 @@
 import streamlit as st
-st.write("""#MyInterface
-thids is my interface using streamlit
+st.write("""#MyInterface using streamlit""")
+st.write("""this is my interface using streamlit
 """)
